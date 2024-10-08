bfef2d7e5cd5e4de56d84edd53550d6adc7b751d
sbx_user1051 <sbx_user1051@169.254.119.77>
2024-10-08 12:50:33 +0000

    Tue Oct 08 12:50:33 UTC 2024

--- src/test/kotlin/solutions/SUM/SumSolutionTest.kt
+++ src/test/kotlin/solutions/SUM/SumSolutionTest.kt
@@ -1,12 +1,20 @@
 package solutions.SUM
 
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.*
 
 class SumSolutionTest {
 
     @Test
     fun sum() {
-        Assertions.assertEquals(2, SumSolution.sum(1, 1))
+        assertEquals(2, SumSolution.sum(1, 1))
+    }
+
+    @Test
+    fun 'negative inputs'(){
+        val exception = assertThrows<IllegalArgumentException>{
+            SumSolution.sum(-1,-1)
+        }
+
+        assertEquals("Sum inputs must be between 0 and 100", exception.message)
     }
 }
