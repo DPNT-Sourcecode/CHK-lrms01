19797aff198805513e3503fcb677417fe7af21d3
sbx_user1051 <sbx_user1051@169.254.119.77>
2024-10-08 13:02:33 +0000

    Tue Oct 08 13:02:33 UTC 2024

--- src/main/kotlin/solutions/SUM/SumSolution.kt
+++ src/main/kotlin/solutions/SUM/SumSolution.kt
@@ -2,10 +2,10 @@ package solutions.SUM
 
 object SumSolution {
     fun sum(x: Int, y: Int): Int {
-        require(x > 0) { "Sum inputs must be between 0 and 100" }
-        require(y > 0) { "Sum inputs must be between 0 and 100" }
+        require(x > 0 && y > 0) { "Sum inputs must be between 0 and 100" }
 
         return x+y
     }
 }
+
 
