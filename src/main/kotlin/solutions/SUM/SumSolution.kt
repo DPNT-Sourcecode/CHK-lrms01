f367149091b01d313c4df714f34f678add4340db
sbx_user1051 <sbx_user1051@169.254.119.77>
2024-10-08 13:05:33 +0000

    Tue Oct 08 13:05:33 UTC 2024

--- src/main/kotlin/solutions/SUM/SumSolution.kt
+++ src/main/kotlin/solutions/SUM/SumSolution.kt
@@ -2,7 +2,7 @@ package solutions.SUM
 
 object SumSolution {
     fun sum(x: Int, y: Int): Int {
-        require(x > 0 && y > 0) { "Sum inputs must be between 0 and 100" }
+        require(x >= 0 && x<= 100 && y >= 0 && y <= 100) { "Sum inputs must be between 0 and 100" }
 
         return x+y
     }
--- src/test/kotlin/solutions/SUM/SumSolutionTest.kt
+++ src/test/kotlin/solutions/SUM/SumSolutionTest.kt
@@ -14,7 +14,22 @@ class SumSolutionTest {
         val exception = assertThrows<IllegalArgumentException>{
             SumSolution.sum(-1,-1)
         }
+        Assertions.assertEquals("Sum inputs must be between 0 and 100", exception.message)
+    }
 
+    @Test
+    fun singleNegative(){
+        val exception = assertThrows<IllegalArgumentException>{
+            SumSolution.sum(10,-1)
+        }
+        Assertions.assertEquals("Sum inputs must be between 0 and 100", exception.message)
+    }
+
+    @Test
+    fun lessThan100(){
+        val exception = assertThrows<IllegalArgumentException>{
+            SumSolution.sum(110,10)
+        }
         Assertions.assertEquals("Sum inputs must be between 0 and 100", exception.message)
     }
 }
