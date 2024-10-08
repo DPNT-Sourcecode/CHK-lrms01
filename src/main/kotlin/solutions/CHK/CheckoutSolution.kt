bf4f954200a77001363c8d9db0f44ad5705568c8
sbx_user1051 <sbx_user1051@169.254.119.77>
2024-10-08 13:53:12 +0000

    Tue Oct 08 13:53:12 UTC 2024

--- src/main/kotlin/solutions/CHK/CheckoutSolution.kt
+++ src/main/kotlin/solutions/CHK/CheckoutSolution.kt
@@ -10,18 +10,31 @@ object CheckoutSolution {
     )
 
     val offers = mapOf(
-        'A' to pair(3, 130),
-        'B' to pair(2, 45)
+        'A' to Pair(3, 130),
+        'B' to Pair(2, 45)
     )
 
     fun checkout(skus: String): Int {    
-        // Ensure all Items in the SKU are capitalised to match the map
-        val upperSKUs = skus.uppercase()
+        // Ensure all Items in the SKU are capitalised and stripped of
+        // whitespace to match the map
+        val adjustedSKUs = skus.uppercase().replace(" ", "").trim()
 
         // Start by Checking All Items in the SKU are valid
+        if(adjustedSKUs.any{ it !in prices.keys }){
+            return -1
+        }
+        
+        // Get a count of each individual char in the SKU
+        val charFreq = adjustedSKUs.groupingBy { it }.eachCount()
 
 
+        //Start calculating the total running cost
+        var totalCost = 0
 
-        TODO("Solution not implemented")
+        charFreq.forEach{
+            println(it)
+        }
+        
+        return -1
     }
 }
